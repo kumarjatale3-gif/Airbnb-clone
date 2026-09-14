db510e1bd7f5bbe34e8bea739c8221c2f6a03f40
Kumar Jatale <kumarjatale3@gmail.com>
2026-09-14 23:52:49 +0530

    changed listing.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -21,7 +21,7 @@ module.exports.showListing = async (req, res) => {
     .populate("Owner");
   if (!listing) {
     req.flash("error", "Listing you requested for does not exists !");
-    res.redirect("/listings");
+    return res.redirect("/listings");
   } else {
     res.render("listings/show.ejs", { listing });
   }
@@ -46,7 +46,7 @@ module.exports.renderEditForm = async (req, res) => {
   const listing = await Listing.findById(id);
   if (!listing) {
     req.flash("error", "Listing you requested for does not exists !");
-    res.redirect("/listings");
+    return res.redirect("/listings");
   } else {
     let originalImageUrl = listing.image.url;
     originalImageUrl = originalImageUrl.replace("/upload","/upload/w_250");
